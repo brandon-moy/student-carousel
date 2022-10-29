9beae7b6bc069dc9be1cd5d510048f4892ccceb2
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-29 23:27:20 +0000

    Modified back of card to have icons linking to linked in and github

--- src/cards.jsx
+++ src/cards.jsx
@@ -8,9 +8,12 @@ export default class Cards extends React.Component {
   }
 
   handleClick(event) {
-    const id = event.target.closest('.card').id;
-    const person = this.state.links.filter(links => links.name === id);
-    window.open(person[0].link);
+    // const id = event.target.closest('.card').id;
+    // const person = this.state.links.filter(links => links.name === id);
+    // window.open(person[0].link);
+    const card = event.target.closest('.card');
+    // eslint-disable-next-line no-console
+    console.log(card);
   }
 
   componentDidMount() {
@@ -31,7 +34,13 @@ export default class Cards extends React.Component {
                     <h1 className="student-name">{image.name}</h1>
                   </div>
                   <div className="back">
-                    <img className="logo" src={image.back}></img>
+                    <p>This is some text</p>
+                    <a className="linkedin-link" href={image.linked}>
+                      <img className="linkedin-icon" src={image.linkIcon}></img>
+                    </a>
+                    <a className="github-link" href={image.github}>
+                      <img className="github-icon" src={image.githubIcon}></img>
+                    </a>
                   </div>
                 </div>
               );
--- src/index.jsx
+++ src/index.jsx
@@ -7,84 +7,108 @@ const cards = [
     number: 0,
     name: 'Markus Doan-Le',
     front: 'images/markus.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/mdoanle',
     linked: 'https://www.linkedin.com/in/markusdoan-le/'
   },
   {
     number: 1,
     name: 'Jiheng Nie',
     front: 'images/jiheng.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/JihengNie',
     linked: 'https://www.linkedin.com/in/jiheng-nie/'
   },
   {
     number: 2,
     name: 'Brandon Moy',
     front: 'images/brandon.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'http://github.com/brandon-moy',
     linked: 'https://www.linkedin.com/in/brandon-k-moy/'
   },
   {
     number: 3,
     name: 'Eugene Park',
     front: 'images/eugene.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/Eugenepark215',
     linked: 'https://www.linkedin.com/in/eugenepark215/'
   },
   {
     number: 4,
     name: 'Anthony Urbina',
     front: 'images/anthony.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/AnthonyUrbina',
     linked: 'https://www.linkedin.com/in/anthonyisaiahurbina/'
   },
   {
     number: 5,
     name: 'Daisy Yoo',
     front: 'images/daisy.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/daisyyoo',
     linked: 'https://www.linkedin.com/in/daisy-yoo/'
   },
   {
     number: 6,
     name: 'Derrick Fleming',
     front: 'images/derrick.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/derrick-fleming',
     linked: 'https://www.linkedin.com/in/derrick-fleming/'
   },
   {
     number: 7,
     name: 'Jake Husband',
     front: 'images/jake.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/jacobhusband',
     linked: 'https://www.linkedin.com/in/jacob-husband/'
   },
   {
     number: 8,
     name: 'Jianni Cariaga',
     front: 'images/jianni.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/jiannicariaga',
     linked: 'https://www.linkedin.com/in/jiannicariaga/'
   },
   {
     number: 9,
     name: 'Quan Pham',
     front: 'images/quan.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/quannampham',
     linked: 'https://www.linkedin.com/in/quannampham/'
   },
   {
     number: 10,
     name: 'Matthew Kweon',
     front: 'images/matt.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/MattKweon',
     linked: 'https://www.linkedin.com/in/matthew-kweon/'
   },
   {
     number: 11,
     name: 'Ashley Ho',
     front: 'images/ashley.png',
-    back: 'images/logo.png',
+    linkIcon: 'images/linkedin.png',
+    githubIcon: 'images/github.svg',
+    github: 'https://github.com/ashleylho',
     linked: 'https://www.linkedin.com/in/ashleylho/'
   }
 ];
