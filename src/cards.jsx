ea2999a52b17708fa9388e1a2d6b288bf939c83b
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-11-01 23:59:46 +0000

    Changed some dom queries to react refs and removed commented out code

--- src/cards.jsx
+++ src/cards.jsx
@@ -5,42 +5,38 @@ export default class Cards extends React.Component {
     super(props);
     this.showBack = this.showBack.bind(this);
     this.showFront = this.showFront.bind(this);
+    this.carouselRef = React.createRef();
     this.carouselCycle = this.carouselCycle.bind(this);
     this.rotationTransition = this.rotationTransition.bind(this);
     this.state = { location: 1, intervalId: null };
   }
 
   showBack(event) {
-    // clearInterval(this.state.intervalId);
-    event.target.closest('.carousel').classList.toggle('rotating');
+    this.carouselRef.current.classList.toggle('rotating');
     const card = event.target.closest('.card-rotate');
     card.classList.toggle('is-flipped');
   }
 
   showFront(event) {
-    event.target.closest('.carousel').classList.toggle('rotating');
+    this.carouselRef.current.classList.toggle('rotating');
     const card = event.target.closest('.card-rotate');
     if (!card.classList.contains('is-flipped')) return;
     card.classList.toggle('is-flipped');
-    // this.carouselCycle();
   }
 
   carouselCycle() {
     const intervalId = setInterval(() => {
       const location = this.state.location + 1;
       const amount = location * 30;
-      const $carousel = document.querySelector('.carousel');
-      $carousel.style.transform = 'rotateY(-' + amount + 'deg)';
+      this.carouselRef.current.style.transform = 'rotateY(-' + amount + 'deg)';
       this.setState({ location });
     }, 3000);
     this.setState({ intervalId });
   }
 
   componentDidMount() {
-    // this.carouselCycle();
     setTimeout(() => {
-      const $carousel = document.querySelector('.carousel');
-      $carousel.style.transform = 'rotateY(-30deg)';
+      this.carouselRef.current.style.transform = 'rotateY(-30deg)';
     }, 0);
   }
 
@@ -55,15 +51,14 @@ export default class Cards extends React.Component {
     if (flippedCards > 0) return;
     const location = this.state.location + 1;
     const amount = location * 30;
-    const $carousel = document.querySelector('.carousel');
-    $carousel.style.transform = 'rotateY(-' + amount + 'deg)';
+    this.carouselRef.current.style.transform = 'rotateY(-' + amount + 'deg)';
     this.setState({ location });
   }
 
   render() {
     return (
       <div className="scene">
-        <div className="carousel rotating" onTransitionEnd={this.rotationTransition}>
+        <div className="carousel rotating" ref={this.carouselRef} onTransitionEnd={this.rotationTransition}>
           {
             this.props.cards.map(image => {
               return (
